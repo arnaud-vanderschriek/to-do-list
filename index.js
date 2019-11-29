d15a9c2d516d702352feb4ce008378940c2718cc
arnaud-vanderschriek <arnaud.vanderschrieck@gmail.com>
2019-11-29 09:03:16 +0100

    add route + basic layout

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const favicon = require('serve-favicon');
 const path = require('path');
 const bodyParser = require('body-parser');
 const session = require('cookie-session');
-const pathRoute = require('./route/router');                             // assignation du fichier router.js a la variable pathRoute
+const pathRoute = require('./route/router'); 
+const fetch = require('node-fetch');                            // assignation du fichier router.js a la variable pathRoute
 
 
 const urlEncoderParser = bodyParser.urlencoded({ extended:false})
@@ -30,13 +31,18 @@ app.use( (req, res, next) => {
 
 // définir la view "main.ejs" comme page principale. => '/'
 app.get('/', (req,res) => {
+    
     res.render('main.ejs', {todo: req.session.todo} );
 })
 
 // ajouter une tache
 app.post('/', urlEncoderParser, (req,res) => {    // urlEncoderParser permet de recupérer l'attribut name du formulaire (par la méthode POST dans ce cas)
-    req.session.todo.push(req.body.name);
-    res.redirect('/');
+    if(req.body.name == ''){
+        res.redirect('/');
+    }else{
+        req.session.todo.push(req.body.name);
+        res.redirect('/');
+    }
 })
 
 app.get('/supprimer/:id', (req,res) =>{
@@ -45,6 +51,29 @@ app.get('/supprimer/:id', (req,res) =>{
     }
     res.redirect('/');
 })
+
+// app.post('/update/:id', (req, res) =>{               // A FAIRE !!!!!!!!!     Fonction Update....
+//     if(req.params.id != ''){
+//         req.body.up = req.params.id 
+//     }
+//     res.redirect('/');
+// })
+
+
+// app.get('/meteo', (req, res) => {
+//     const api_url = "https://api.darksky.net/forecast/04b59744b4cb848ebf7be229b519487a/37.8267,-122.4233";
+//     fetch(api_url)
+//     .then(res => res.json())
+//     .then(data => {
+//     res.json(data);
+//     console.log(data);
+// })
+// })
+// const api_url = "https://api.darksky.net/forecast/04b59744b4cb848ebf7be229b519487a/37.8267,-122.4233";
+// const fetch_response = await fetch(api_url);
+// console.log(fetch_response);
+// const resp = await fetch_response.json();
+// console.log(resp);
 
 app.use( (req, res) => {
     res.redirect('/');
--- route/router.js
+++ route/router.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const router = express.Router();            // utilisation de la methode "Router" d'"Express"
+const router = express.Router();           // utilisation de la methode "Router" d'"Express"
+const fetch = require('node-fetch');
 
 
-router.get('/meteo', (req,res) => {         // app.get => router.get
-    res.render('meteo.ejs');
+router.get('/meteo', (req,res) => {        // app.get => router.get
+    const api_url = "https://api.darksky.net/forecast/04b59744b4cb848ebf7be229b519487a/37.8267,-122.4233";
+    fetch(api_url)
+    .then(res => res.json())
+    .then(data => {
+    res.json(data);
+    })
 })
 
 router.get('/gif', (req, res) => {
